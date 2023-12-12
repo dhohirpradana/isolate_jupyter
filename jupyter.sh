d8f7d5a144e842693adb5b07d7b7ed7694827364
Dhohir Pradana <dhohirkudus@gmail.com>
2023-12-13 00:14:32 +0700

    feat: docker stack

--- jupyter.sh
+++ jupyter.sh
@@ -2,8 +2,7 @@
 
 # Check if arguments are provided
 if [ $# -lt 2 ]; then
-    # echo "Usage: $0 <service_name> <port>"
-    echo "400"
+    echo "Usage: $0 <service_name> <port>"
     exit 1
 fi
 
@@ -12,59 +11,45 @@ service_name=$1
 port=$2
 
 # Check if container/service already exists
-if [ "$(docker ps -a -q -f name=$service_name)" ]; then
-    # echo "Service '$service_name' already exists. Please use a different service name."
-    echo "409"
+if [ "$(docker ps -q -f name=$service_name)" ]; then
+    echo "Service '$service_name' already exists. Please use a different service name."
     exit 1
 fi
 
 # Check if the port is already in use
 if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; then
-    echo "406"  # Port already in use
+    echo "Port $port is already in use."
     exit 1
 fi
 
-# Function to check if jtoken is empty
-is_jtoken_empty() {
-    [ -z "$jtoken" ]
-}
-
-# Function to wait until jtoken is not empty
-wait_for_jtoken() {
-    local timeout=30  # Maximum wait time in seconds
-    local interval=1   # Interval to check in seconds
-    local waited=0
-
-    while is_jtoken_empty && [ $waited -lt $timeout ]; do
-        sleep $interval
-        jtoken=$(docker logs "$service_name" | grep "token=" | awk -F"token=" '{print $2}' | awk '{print $1}' | head -n 1)
-        waited=$((waited + interval))
-    done
-}
-
 # Create docker-compose.yml
 cat > docker-compose.yml <<EOF
+version: '3.8'
+
 services:
   $service_name:
-    image: jupyter/scipy-notebook:70178b8e48d7
-    container_name: $service_name
+    image: 103.127.97.93:5000/jupyter-spark
+    deploy:
+      replicas: 1
     ports:
       - "$port:8888"
     volumes:
       - jupyter$service_name:/home/jovyan/work
     environment:
       - JUPYTER_ENABLE_LAB=yes
-    tty: true
-    stdin_open: true
+      - GRANT_SUDO=yes
 
 volumes:
   jupyter$service_name:
-
 EOF
 
-docker compose up -d
+# Deploy the stack
+docker stack deploy -c docker-compose.yml $service_name
 
-# Wait for jtoken to be available
-wait_for_jtoken
+# Wait for a while for the service to start (adjust this timing according to your setup)
+sleep 10
 
-echo "$jtoken"
+# Get the token from the logs of one of the containers
+jtoken=$(docker service logs $service_name_$service_name | grep "token=" | awk -F"token=" '{print $2}' | awk '{print $1}' | head -n 1)
+
+echo "Token for $service_name service is: $jtoken"
